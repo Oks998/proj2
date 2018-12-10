ae6a6dd0b07c20bde7383ad890d4098ec4686e7c
Oks998 <44585037+Oks998@users.noreply.github.com>
2018-12-10 22:16:41 +0300

    Add files via upload

---
+++ admin/index1.php
@@ -0,0 +1,116 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "eshop";
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>eshop</title>
+    <link rel="stylesheet" href="css/style2.css">
+</head>
+<body>
+<header>
+<ul>
+	<li><a href="/">ГЛАВНАЯ</a></li>
+	<li><a href="admin/index1.php">ВСЕ ТОВАРЫ</a></li>
+	<li><a href="/">КОРЗИНА</a></li>
+	<li><a href="/">КАК С НАМИ СВЯЗАТЬСЯ</a></li>
+</ul>
+</header>
+<main>
+    <div class="mini-cart"></div>
+    <div class="goods-out"></div>
+</main>
+<div class="tov">
+	
+<!--
+<div class="cart">
+   <p class="name">451 градус по Фаренгейту</p>
+    <img src="img/qw.jpg" alt="">
+    <div class="cost">220 грн.</div>
+    <div class="description">Научно-фантастический роман-антиутопия Рэя Брэдбери </div>
+    <button class="add-to-cart">Купить</button>
+</div>
+
+<div class="cart1">
+   <p class="name">До встречи с тобой </p>
+    <img src="img/as.jpg" alt="">
+    <div class="cost">340 грн.</div>
+    <div class="description">Любовный роман </div>
+    <button class="add-to-cart">Купить</button>
+</div>
+
+<div class="cart2">
+   <p class="name">Гордость и предубеждение</p>
+    <img src="img/zx.jpg" alt="">
+    <div class="cost">140 грн.</div>
+    <div class="description">Роман </div>
+    <button class="add-to-cart">Купить</button>
+</div>
+
+<div class="cart3">
+   <p class="name">Шопенгауэр как лекарство</p>
+    <img src="img/er.jpg" alt="">
+    <div class="cost">180 грн.</div>
+    <div class="description">Художественная литература (Психологический роман) </div>
+    <button class="add-to-cart">Купить</button>
+</div>
+
+<div class="cart4">
+   <p class="name">Игра престолов</p>
+    <img src="img/sd.jpg" alt="">
+    <div class="cost">550 грн.</div>
+    <div class="description"> Роман в жанре фэнтези </div>
+    <button class="add-to-cart">Купить</button>
+</div>
+
+<div class="cart5">
+   <p class="name">Властелин колец</p>
+    <img src="img/rew.jpg" alt="">
+    <div class="cost">425 грн.</div>
+    <div class="description"> Фэнтези, Приключенческая художественная литература </div>
+    <button class="add-to-cart">Купить</button>
+</div>
+
+<div class="cart6">
+   <p class="name">Дорогой Джон</p>
+    <img src="img/cvb.jpeg" alt="">
+    <div class="cost">270 грн. </div>
+    <div class="description"> Художественная литература </div>
+    <button class="add-to-cart">Купить</button>
+</div>
+
+<div class="cart7">
+   <p class="name">Психология влияния</p>
+    <img src="img/tg.jpg" alt="">
+    <div class="cost">145 грн.</div>
+    <div class="description"> Психология </div>
+    <button class="add-to-cart">Купить</button>
+</div>
+
+<div class="cart8">
+   <p class="name">Поступай как женщина</p>
+    <img src="img/hn.jpg" alt="">
+    <div class="cost">210 грн.</div>
+    <div class="description"> Психология </div>
+    <button class="add-to-cart">Купить</button>
+</div>
+
+<div class="cart9">
+   <p class="name">Цветы для Элджернона</p>
+    <img src="img/bbv.jpg" alt="">
+    <div class="cost">180 грн.</div>
+    <div class="description">Научная фантастика </div>
+    <button class="add-to-cart">Купить</button>
+</div>-->
+
+</div>
+<footer></footer>
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/admin.js"></script>
+</body>
+</html>
